74de86a7a961734b840236ed596799a689ba9d3e
juh9870 <juh9870@ukr.net>
2024-12-27 00:42:23 +0200

    feat: type docs and renamed Optional enum variants

--- dbe_ui/src/main_toolbar/docs.rs
+++ dbe_ui/src/main_toolbar/docs.rs
@@ -1,6 +1,8 @@
 use crate::DbeApp;
+use dbe_backend::etype::eobject::EObject;
 use dbe_backend::graph::node::{get_node_factory, NodeFactory};
-use dbe_backend::project::docs::{DocsDescription, NodeDocs, NodeIODocs};
+use dbe_backend::project::docs::{DocsDescription, NodeDocs, TypeDocs};
+use dbe_backend::registry::{EObjectType, ETypesRegistry};
 use egui::{RichText, Ui, Widget, WidgetText};
 use egui_commonmark::CommonMarkCache;
 use inline_tweak::tweak;
@@ -28,7 +30,14 @@ pub fn docs_tab(ui: &mut Ui, app: &mut DbeApp) {
 
         ui.collapsing("Types", |ui| {
             for (type_id, docs) in &project.docs.types {
-                todo!("Show type docs");
+                let Some(ty) = project.registry.get_object(type_id) else {
+                    continue;
+                };
+                egui::CollapsingHeader::new(ty.title(&project.registry))
+                    .id_salt(type_id)
+                    .show(ui, |ui| {
+                        type_docs(ui, &project.registry, ty, docs);
+                    });
             }
         })
     });
@@ -50,6 +59,19 @@ fn show_description(ui: &mut Ui, docs: &impl DocsDescription, md_cache: &mut Com
     }
 }
 
+fn show_collapsing_description(
+    ui: &mut Ui,
+    docs: &impl DocsDescription,
+    md_cache: &mut CommonMarkCache,
+    title: &str,
+) {
+    egui::CollapsingHeader::new(RichText::new(title).strong().size(tweak!(14.0)))
+        .default_open(true)
+        .show(ui, |ui| {
+            show_description(ui, docs, md_cache);
+        });
+}
+
 pub fn node_docs(ui: &mut Ui, node: &dyn NodeFactory, docs: &NodeDocs) {
     egui::Label::new(RichText::new(&docs.title).heading())
         .ui(ui)
@@ -66,27 +88,45 @@ pub fn node_docs(ui: &mut Ui, node: &dyn NodeFactory, docs: &NodeDocs) {
     show_description(ui, docs, &mut md_cache);
 
     ui.style_mut().visuals.indent_has_left_vline = false;
-
-    let mut docs_io = |ui: &mut Ui, docs: &NodeIODocs| {
-        egui::CollapsingHeader::new(RichText::new(&docs.title).strong().size(tweak!(14.0)))
-            .default_open(true)
-            .show(ui, |ui| {
-                show_description(ui, docs, &mut md_cache);
-            });
-    };
-
     if !docs.inputs.is_empty() {
         ui.label(RichText::new("Inputs").heading());
         ui.separator();
         for docs in &docs.inputs {
-            docs_io(ui, docs)
+            show_collapsing_description(ui, docs, &mut md_cache, &docs.title);
         }
     }
     if !docs.outputs.is_empty() {
         ui.label(RichText::new("Outputs").heading());
         ui.separator();
         for docs in &docs.outputs {
-            docs_io(ui, docs)
+            show_collapsing_description(ui, docs, &mut md_cache, &docs.title);
+        }
+    }
+}
+
+pub fn type_docs(ui: &mut Ui, registry: &ETypesRegistry, ty: &EObjectType, docs: &TypeDocs) {
+    egui::Label::new(RichText::new(ty.title(registry)).heading())
+        .ui(ui)
+        .on_hover_text(format!("ID: {}", ty.ident()));
+
+    ui.separator();
+
+    let mut md_cache = CommonMarkCache::default();
+
+    show_description(ui, docs, &mut md_cache);
+
+    ui.style_mut().visuals.indent_has_left_vline = false;
+    if ty.as_enum().is_some() {
+        ui.label(RichText::new("Variants").heading());
+        ui.separator();
+        for docs in &docs.variants {
+            show_collapsing_description(ui, docs, &mut md_cache, &docs.id);
+        }
+    } else {
+        ui.label(RichText::new("Fields").heading());
+        ui.separator();
+        for docs in &docs.fields {
+            show_collapsing_description(ui, docs, &mut md_cache, &docs.id);
         }
     }
 }
