0b9e3ee76b33945ec0d5d9b34b4d768e4bf58182
juh9870 <juh9870@ukr.net>
2025-01-11 20:55:36 +0200

    feat: rounded region corners

--- utils/src/math.rs
+++ utils/src/math.rs
@@ -1,1 +1,63 @@
+use emath::{Pos2, Vec2};
+use num_traits::Float;
+use strum::EnumIs;
+
 pub mod convex_hull_2d;
+
+pub mod minkowski;
+
+const EPS: f32 = 1e-5;
+
+/// Calculate the cross product of two vectors
+pub fn cross(a: Vec2, b: Vec2) -> f32 {
+    a.x * b.y - a.y * b.x
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, EnumIs)]
+pub enum WindingDirection {
+    Clockwise,
+    CounterClockwise,
+}
+
+/// Calculate the winding direction of a polygon
+///
+/// Returns None if the winding direction cannot be determined (e.g. all points
+/// are collinear)
+pub fn winding_direction(points: &[Pos2]) -> Option<WindingDirection> {
+    let mut dx = (points[1] - points[0]).normalized();
+    for i in 1..points.len() {
+        let next_dx = (points[(i + 1) % points.len()] - points[i]).normalized();
+
+        if vec_approx_eq(next_dx, Vec2::ZERO) {
+            continue;
+        }
+
+        if vec_approx_eq(dx, Vec2::ZERO) {
+            dx = next_dx;
+            continue;
+        }
+
+        let cross = cross(dx, next_dx);
+
+        // Cross product is significant enough to determine winding direction
+        if cross.abs() > 0.015 {
+            match cross.signum() {
+                1.0 => return Some(WindingDirection::CounterClockwise),
+                -1.0 => return Some(WindingDirection::Clockwise),
+                _ => {
+                    unreachable!("Cross product is not zero, but signum is zero")
+                }
+            }
+        }
+    }
+
+    None
+}
+
+fn approx_eq(a: f32, b: f32) -> bool {
+    (a - b).abs() < (a * EPS).max(EPS)
+}
+
+fn vec_approx_eq(a: Vec2, b: Vec2) -> bool {
+    approx_eq(a.x, b.x) && approx_eq(a.y, b.y)
+}
---
+++ utils/src/math/minkowski.rs
@@ -0,0 +1,65 @@
+//! ported from https://cp-algorithms.com/geometry/minkowski.html#implementation
+
+use crate::math::{cross, winding_direction, WindingDirection};
+use emath::Pos2;
+
+/// Calculate the Minkowski sum of two convex polygons
+///
+/// The input polygons must be convex and have vertices in counter-clockwise order.
+pub fn minkowski(mut p: Vec<Pos2>, mut q: Vec<Pos2>) -> Vec<Pos2> {
+    // check winding direction but only when debug_assertions are enabled
+    debug_assert_eq!(
+        winding_direction(&p),
+        Some(WindingDirection::CounterClockwise)
+    );
+    debug_assert_eq!(
+        winding_direction(&q),
+        Some(WindingDirection::CounterClockwise)
+    );
+
+    // the first vertex must be the lowest
+    reorder_polygon(&mut p);
+    reorder_polygon(&mut q);
+    // we must ensure cyclic indexing
+    p.push(p[0]);
+    p.push(p[1]);
+    q.push(q[0]);
+    q.push(q[1]);
+    // main part
+    let mut result = vec![];
+    let mut i = 0;
+    let mut j = 0;
+    while i < p.len() - 2 || j < q.len() - 2 {
+        result.push(p[i] + q[j].to_vec2());
+        let cross = cross(
+            (p[i + 1] - p[i].to_vec2()).to_vec2(),
+            (q[j + 1] - q[j].to_vec2()).to_vec2(),
+        );
+        if cross >= 0.0 && i < p.len() - 2 {
+            i += 1;
+        }
+        if cross <= 0.0 && j < q.len() - 2 {
+            j += 1;
+        }
+
+        #[cfg(debug_assertions)]
+        if result.len() > 1000000 {
+            panic!("Infinite loop");
+        }
+    }
+
+    result
+}
+
+fn reorder_polygon(points: &mut Vec<Pos2>) {
+    let mut pos = 0;
+    for i in 1..points.len() {
+        if points[i].y < points[pos].y
+            || (points[i].y == points[pos].y && points[i].x < points[pos].x)
+        {
+            pos = i
+        }
+    }
+
+    points.rotate_left(pos)
+}
