01944e1276325c07d44963e3e43289cb22c41e19
juh9870 <juh9870@ukr.net>
2025-01-26 15:18:06 +0200

    fix: don't allow invalid files in set_field node

---
+++ .tinychange/upgradable-encompassing-structure-24fca22.md
@@ -0,0 +1,4 @@
+- Author: juh9870
+- Kind: Fixed
+---
+`set_field` creating fields with invalid types
--- dbe_backend/src/graph/node/functional/raw_manip.rs
+++ dbe_backend/src/graph/node/functional/raw_manip.rs
@@ -1,7 +1,17 @@
+use crate::etype::eitem::EItemInfo;
+use crate::graph::node::ports::{port_types_compatible, NodePortType};
 use crate::registry::ETypesRegistry;
 use crate::value::EValue;
 use miette::{bail, miette};
+use strum::EnumIs;
 use ustr::ustr;
+
+#[derive(Debug, Copy, Clone, EnumIs)]
+pub enum SwapValueResult {
+    Swapped,
+    InvalidType,
+    FieldNotFound,
+}
 
 /// Swaps value of a field in a struct or an enum.
 pub(super) fn swap_value(
@@ -9,12 +19,29 @@ pub(super) fn swap_value(
     obj: &mut EValue,
     field: &str,
     value: &mut EValue,
-) -> miette::Result<bool> {
+) -> miette::Result<SwapValueResult> {
     match obj {
         EValue::Struct { fields, ident } => {
             if let Some(field_value) = fields.get_mut(&ustr(field)) {
+                let in_info = EItemInfo::simple_type(value.ty());
+                let out_info = EItemInfo::simple_type(field_value.ty());
+
+                if !port_types_compatible(registry, &in_info, &out_info) {
+                    return Ok(SwapValueResult::InvalidType);
+                }
+
+                let in_port: NodePortType = in_info.into();
+                let out_port: NodePortType = out_info.into();
+
+                *value = NodePortType::convert_value(
+                    registry,
+                    &in_port,
+                    &out_port,
+                    std::mem::replace(value, EValue::Null),
+                )?;
+
                 std::mem::swap(field_value, value);
-                return Ok(true);
+                return Ok(SwapValueResult::Swapped);
             }
 
             let data = registry
@@ -30,12 +57,13 @@ pub(super) fn swap_value(
                     .get_mut(&inline_field.name)
                     .ok_or_else(|| miette!("!!INTERNAL ERROR!! field should be present"))?;
 
-                if swap_value(registry, inline_value, field, value)? {
-                    return Ok(true);
+                let result = swap_value(registry, inline_value, field, value)?;
+                if !result.is_field_not_found() {
+                    return Ok(result);
                 }
             }
 
-            Ok(false)
+            Ok(SwapValueResult::FieldNotFound)
         }
         EValue::Enum { data, .. } => swap_value(registry, data, field, value),
         _ => bail!("value is not a struct or an enum"),
