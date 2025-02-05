dbed5115a89b732b7bd57d1852ee11a1751574fa
juh9870 <juh9870@ukr.net>
2025-02-05 20:04:18 +0200

    feat: more list nodes

--- dbe_backend/src/graph/node/functional.rs
+++ dbe_backend/src/graph/node/functional.rs
@@ -807,6 +807,16 @@ pub fn functional_nodes() -> Vec<Arc<dyn NodeFactory>> {
             &["list"],
         ),
         functional_node(
+            |_: C, mut a: Vec<GenericValue<0>>, b: Vec<GenericValue<0>>| {
+                a.extend(b);
+                a
+            },
+            "list_concat",
+            &["a", "b"],
+            &["result"],
+            &["list"],
+        ),
+        functional_node(
             |_: C, mut list: Vec<GenericValue<0>>, index: ENumber, item: GenericValue<0>| {
                 list.insert(index.0 as usize, item);
                 list
@@ -834,6 +844,13 @@ pub fn functional_nodes() -> Vec<Arc<dyn NodeFactory>> {
             "list_remove",
             &["list", "index"],
             &["item", "list"],
+            &["list"],
+        ),
+        functional_node(
+            |_: C, list: Vec<GenericValue<0>>, item: GenericValue<0>| list.contains(&item),
+            "list_contains",
+            &["list", "item"],
+            &["contains"],
             &["list"],
         ),
         side_effects_node(
